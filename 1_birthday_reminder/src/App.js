2319a0e60a1aa4edb539a7fea358f17397587c45
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2020-10-28 01:02:25 +0300

    Birthday reminder finished

--- 1_birthday_reminder/src/App.js
+++ 1_birthday_reminder/src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
-function App() {
-  return <h2>reminder project setup</h2>;
-}
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
+const App = () => {
+  const [people, setPeople] = useState(data);
+
+  const clearPeopleHandler = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={clearPeopleHandler}>Clear All</button>
+      </section>
+    </main>
+  );
+};
 
 export default App;
