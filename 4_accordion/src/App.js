12067c1c438d0e633666052894791dcce4af6a33
Alper Mehmet Özdemir <amehmet97@hotmail.com>
2020-10-28 11:21:51 +0300

    Accrodion project finished

--- 4_accordion/src/App.js
+++ 4_accordion/src/App.js
@@ -1,8 +1,20 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
-}
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
+const App = () => {
+  const [questions, setQuestions] = useState(data);
+
+  const questionList = questions.map((q) => (
+    <SingleQuestion key={q.id} {...q} />
+  ));
+
+  return (
+    <main className="container">
+      <h3>Questions and answers about login</h3>
+      <section className="info">{questionList}</section>
+    </main>
+  );
+};
 
 export default App;
